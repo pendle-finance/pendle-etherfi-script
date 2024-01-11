9aa4fb5e5edfa5616832e1a4237cade41c345ef4
ngfam <ngfamgithub@gmail.com>
2024-01-11 17:04:06 +0700

    Added tokens tracking for EETH

--- src/consts.js
+++ src/consts.js
@@ -55,26 +55,26 @@ const YT_INDEX_QUERY = {
 };
 
 const LIQUID_LOCKERS = [
-  {
-    // penpie
-    address: "0x6e799758cee75dae3d84e09d40dc416ecf713652",
-    receiptToken: "0x866361b922cb7753a5035bf5dd68c02817fc7a39",
-  },
-  {
-    // equilibira
-    address: "0x64627901dadb46ed7f275fd4fc87d086cff1e6e3",
-    receiptToken: "0x9297155e19333d65054e81bdbc10b168ce8f1378",
-  },
-  {
-    // stakedao
-    address: "0xd8fa8dc5adec503acc5e026a98f32ca5c1fa289a",
-    receiptToken: "0xc9a675fffb6f8f821d9cd92c4036c61531b4eaeb",
-  },
+  // {
+  //   // penpie
+  //   address: "0x6e799758cee75dae3d84e09d40dc416ecf713652",
+  //   receiptToken: "0x866361b922cb7753a5035bf5dd68c02817fc7a39",
+  // },
+  // {
+  //   // equilibira
+  //   address: "0x64627901dadb46ed7f275fd4fc87d086cff1e6e3",
+  //   receiptToken: "0x9297155e19333d65054e81bdbc10b168ce8f1378",
+  // },
+  // {
+  //   // stakedao
+  //   address: "0xd8fa8dc5adec503acc5e026a98f32ca5c1fa289a",
+  //   receiptToken: "0xc9a675fffb6f8f821d9cd92c4036c61531b4eaeb",
+  // },
 ];
 
-const SY = "0xe05082b184a34668cd8a904d85fa815802bbb04c";
-const YT = "0xed97f94dd94255637a054098604e0201c442a3fd";
-const LP = "0xc9becdbc62efb867cb52222b34c187fb170379c6";
+const SY = "0xac0047886a985071476a1186be89222659970d65";
+const YT = "0xfb35fd0095dd1096b1ca49ad44d8c5812a201677";
+const LP = "0xf32e58f92e60f4b0a37a69b95d642a471365eae8";
 
 const MARKET_IFACE = new ethers.utils.Interface(PendleMarketABI);
 
