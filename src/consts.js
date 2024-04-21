d6612a99d998b760e3d5251a7b17bd39da305d32
ngfam <ngfamgithub@gmail.com>
2024-04-21 11:47:21 +0700

    Zircuit pool configuration

--- src/consts.js
+++ src/consts.js
@@ -6,29 +6,22 @@ const LIQUID_LOCKERS = [
   {
     // penpie
     address: "0x6e799758cee75dae3d84e09d40dc416ecf713652",
-    receiptToken: "0x2da4401616dc5668100decfaf579229233b4ec1c",
-    lpToken: "0xf32e58f92e60f4b0a37a69b95d642a471365eae8",
-    deployedBlock: 18983977
+    receiptToken: "0xeb808b323d8b9f2e2424b83e8a08c5f2633c9254",
+    lpToken: "0xe26d7f9409581f606242300fbfe63f56789f2169",
+    deployedBlock: 19531959
   },
   {
     // equilibira
     address: "0x64627901dadb46ed7f275fd4fc87d086cff1e6e3",
-    receiptToken: "0x17ea39035ad2cb5d8e2e005349ff23bb52d1c8b7",
-    lpToken: "0xf32e58f92e60f4b0a37a69b95d642a471365eae8",
-    deployedBlock: 18976214    
-  },
-  {  
-    // stakedao
-    address: "0xd8fa8dc5adec503acc5e026a98f32ca5c1fa289a",
-    receiptToken: "0xc6bb9d3d4c980b53c31f6ffb998bea7e74029954",
-    lpToken: "0xf32e58f92e60f4b0a37a69b95d642a471365eae8",
-    deployedBlock: 19027272
+    receiptToken: "0xb68bba67c943665343d388302aa4c82754d4bd34",
+    lpToken: "0xe26d7f9409581f606242300fbfe63f56789f2169",
+    deployedBlock: 19536683    
   },
 ];
 
-const SY = "0xac0047886a985071476a1186be89222659970d65";
-const YT = "0xfb35fd0095dd1096b1ca49ad44d8c5812a201677";
-const LP = "0xf32e58f92e60f4b0a37a69b95d642a471365eae8";
+const SY = "0xd7df7e085214743530aff339afc420c7c720bfa7";
+const YT = "0x7c2d26182adeef96976035986cf56474fec03bda";
+const LP = "0xe26d7f9409581f606242300fbfe63f56789f2169";
 
 const PENDLE_TREASURY = "0x8270400d528c34e1596ef367eedec99080a1b592";
 
