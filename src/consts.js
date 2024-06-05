7b8fb7dd10fd054786292accc4890ee0ec67704e
ngfam <ngfamgithub@gmail.com>
2024-06-05 09:47:39 +0700

    Karak weETH config

--- src/consts.js
+++ src/consts.js
@@ -6,29 +6,29 @@ const LIQUID_LOCKERS = [
   {
     // penpie
     address: "0x6e799758cee75dae3d84e09d40dc416ecf713652",
-    receiptToken: "0xd361ff93ad7112ddf3c3bded94bab4eb364e31cd",
-    lpToken: "0xc8edd52d0502aa8b4d5c77361d4b3d300e8fc81c",
-    deployedBlock: 19881498
+    receiptToken: "0xc683e9a72a2cc87b2110cf680705a21a637fe04e",
+    lpToken: "0x18bafcabf2d5898956ae6ac31543d9657a604165",
+    deployedBlock: 20019723
   },
   {
     // equilibira
     address: "0x64627901dadb46ed7f275fd4fc87d086cff1e6e3",
-    receiptToken: "0x93462b02cf30f93e6635e3acacc867744a7078da",
-    lpToken: "0xc8edd52d0502aa8b4d5c77361d4b3d300e8fc81c",
-    deployedBlock: 19910313    
+    receiptToken: "0xb55a1785a2edaddad3d373b4ecf5916b9d066733",
+    lpToken: "0x18bafcabf2d5898956ae6ac31543d9657a604165",
+    deployedBlock: 19975459    
   },
   {  
     // stakedao
     address: "0xd8fa8dc5adec503acc5e026a98f32ca5c1fa289a",
-    receiptToken: "0x738a5c929ce8af03641776d6681c11c8bab54992",
-    lpToken: "0xc8edd52d0502aa8b4d5c77361d4b3d300e8fc81c",
-    deployedBlock: 19027272
+    receiptToken: "0x0b493988b2425218068767f18dd1ecf2f0084c3d",
+    lpToken: "0x18bafcabf2d5898956ae6ac31543d9657a604165",
+    deployedBlock: 19982914
   },
 ];
 
-const SY = "0xac0047886a985071476a1186be89222659970d65";
-const YT = "0xa54df645a042d24121a737daa89a57ebf8e0b71c";
-const LP = "0xc8edd52d0502aa8b4d5c77361d4b3d300e8fc81c";
+const SY = "0x8db42af6b2de9e8aee47f3423570d8e9c3873796";
+const YT = "0x5439c3ef0072e4a19c44478cdf947f5d957e66c7";
+const LP = "0x18bafcabf2d5898956ae6ac31543d9657a604165";
 
 const PENDLE_TREASURY = "0x8270400d528c34e1596ef367eedec99080a1b592";
 
